f8a6f9d1275289c80d09c7eb1fe960313faaf519
Restyled.io <commits@restyled.io>
2021-05-05 14:45:17 +0000

    Restyled by yapf

--- PublicDataReader/Seoul/transportation.py
+++ PublicDataReader/Seoul/transportation.py
@@ -60,16 +60,14 @@ class TransInfo:
                     except:
                         globals()[variable] = np.nan
                 data = pd.DataFrame(
-                    [
-                        [
-                            USE_DT,
-                            LINE_NUM,
-                            SUB_STA_NM,
-                            RIDE_PASGR_NUM,
-                            ALIGHT_PASGR_NUM,
-                            WORK_DT,
-                        ]
-                    ],
+                    [[
+                        USE_DT,
+                        LINE_NUM,
+                        SUB_STA_NM,
+                        RIDE_PASGR_NUM,
+                        ALIGHT_PASGR_NUM,
+                        WORK_DT,
+                    ]],
                     columns=variables,
                 )
                 df = pd.concat([df, data])
@@ -100,7 +98,9 @@ class TransInfo:
 
             # Open API 서비스 제공처 오류
             else:
-                print(f">>> {te[0].find('CODE').text} {te[0].find('MESSAGE').text}")
+                print(
+                    f">>> {te[0].find('CODE').text} {te[0].find('MESSAGE').text}"
+                )
         # 전체 자료 건 수
         n_data = xmlsoup.findAll("list_total_count")[0].text
         print(f">>> 전체 자료 건 수: {n_data}")
@@ -194,65 +194,63 @@ class TransInfo:
                     except:
                         globals()[variable] = np.nan
                 data = pd.DataFrame(
-                    [
-                        [
-                            USE_MON,
-                            BUS_ROUTE_NO,
-                            BUS_ROUTE_NM,
-                            STND_BSST_ID,
-                            BSST_ARS_NO,
-                            BUS_STA_NM,
-                            MIDNIGHT_RIDE_NUM,
-                            MIDNIGHT_ALIGHT_NUM,
-                            ONE_RIDE_NUM,
-                            ONE_ALIGHT_NUM,
-                            TWO_RIDE_NUM,
-                            TWO_ALIGHT_NUM,
-                            THREE_RIDE_NUM,
-                            THREE_ALIGHT_NUM,
-                            FOUR_RIDE_NUM,
-                            FOUR_ALIGHT_NUM,
-                            FIVE_RIDE_NUM,
-                            FIVE_ALIGHT_NUM,
-                            SIX_RIDE_NUM,
-                            SIX_ALIGHT_NUM,
-                            SEVEN_RIDE_NUM,
-                            SEVEN_ALIGHT_NUM,
-                            EIGHT_RIDE_NUM,
-                            EIGHT_ALIGHT_NUM,
-                            NINE_RIDE_NUM,
-                            NINE_ALIGHT_NUM,
-                            TEN_RIDE_NUM,
-                            TEN_ALIGHT_NUM,
-                            ELEVEN_RIDE_NUM,
-                            ELEVEN_ALIGHT_NUM,
-                            TWELVE_RIDE_NUM,
-                            TWELVE_ALIGHT_NUM,
-                            THIRTEEN_RIDE_NUM,
-                            THIRTEEN_ALIGHT_NUM,
-                            FOURTEEN_RIDE_NUM,
-                            FOURTEEN_ALIGHT_NUM,
-                            FIFTEEN_RIDE_NUM,
-                            FIFTEEN_ALIGHT_NUM,
-                            SIXTEEN_RIDE_NUM,
-                            SIXTEEN_ALIGHT_NUM,
-                            SEVENTEEN_RIDE_NUM,
-                            SEVENTEEN_ALIGHT_NUM,
-                            EIGHTEEN_RIDE_NUM,
-                            EIGHTEEN_ALIGHT_NUM,
-                            NINETEEN_RIDE_NUM,
-                            NINETEEN_ALIGHT_NUM,
-                            TWENTY_RIDE_NUM,
-                            TWENTY_ALIGHT_NUM,
-                            TWENTY_ONE_RIDE_NUM,
-                            TWENTY_ONE_ALIGHT_NUM,
-                            TWENTY_TWO_RIDE_NUM,
-                            TWENTY_TWO_ALIGHT_NUM,
-                            TWENTY_THREE_RIDE_NUM,
-                            TWENTY_THREE_ALIGHT_NUM,
-                            WORK_DT,
-                        ]
-                    ],
+                    [[
+                        USE_MON,
+                        BUS_ROUTE_NO,
+                        BUS_ROUTE_NM,
+                        STND_BSST_ID,
+                        BSST_ARS_NO,
+                        BUS_STA_NM,
+                        MIDNIGHT_RIDE_NUM,
+                        MIDNIGHT_ALIGHT_NUM,
+                        ONE_RIDE_NUM,
+                        ONE_ALIGHT_NUM,
+                        TWO_RIDE_NUM,
+                        TWO_ALIGHT_NUM,
+                        THREE_RIDE_NUM,
+                        THREE_ALIGHT_NUM,
+                        FOUR_RIDE_NUM,
+                        FOUR_ALIGHT_NUM,
+                        FIVE_RIDE_NUM,
+                        FIVE_ALIGHT_NUM,
+                        SIX_RIDE_NUM,
+                        SIX_ALIGHT_NUM,
+                        SEVEN_RIDE_NUM,
+                        SEVEN_ALIGHT_NUM,
+                        EIGHT_RIDE_NUM,
+                        EIGHT_ALIGHT_NUM,
+                        NINE_RIDE_NUM,
+                        NINE_ALIGHT_NUM,
+                        TEN_RIDE_NUM,
+                        TEN_ALIGHT_NUM,
+                        ELEVEN_RIDE_NUM,
+                        ELEVEN_ALIGHT_NUM,
+                        TWELVE_RIDE_NUM,
+                        TWELVE_ALIGHT_NUM,
+                        THIRTEEN_RIDE_NUM,
+                        THIRTEEN_ALIGHT_NUM,
+                        FOURTEEN_RIDE_NUM,
+                        FOURTEEN_ALIGHT_NUM,
+                        FIFTEEN_RIDE_NUM,
+                        FIFTEEN_ALIGHT_NUM,
+                        SIXTEEN_RIDE_NUM,
+                        SIXTEEN_ALIGHT_NUM,
+                        SEVENTEEN_RIDE_NUM,
+                        SEVENTEEN_ALIGHT_NUM,
+                        EIGHTEEN_RIDE_NUM,
+                        EIGHTEEN_ALIGHT_NUM,
+                        NINETEEN_RIDE_NUM,
+                        NINETEEN_ALIGHT_NUM,
+                        TWENTY_RIDE_NUM,
+                        TWENTY_ALIGHT_NUM,
+                        TWENTY_ONE_RIDE_NUM,
+                        TWENTY_ONE_ALIGHT_NUM,
+                        TWENTY_TWO_RIDE_NUM,
+                        TWENTY_TWO_ALIGHT_NUM,
+                        TWENTY_THREE_RIDE_NUM,
+                        TWENTY_THREE_ALIGHT_NUM,
+                        WORK_DT,
+                    ]],
                     columns=variables,
                 )
                 df = pd.concat([df, data])
@@ -263,17 +261,15 @@ class TransInfo:
             df.index = range(len(df))
 
             # 숫자 형 변환
-            cols = df.columns.drop(
-                [
-                    "USE_MON",
-                    "BUS_ROUTE_NO",
-                    "BUS_ROUTE_NM",
-                    "STND_BSST_ID",
-                    "BSST_ARS_NO",
-                    "BUS_STA_NM",
-                    "WORK_DT",
-                ]
-            )
+            cols = df.columns.drop([
+                "USE_MON",
+                "BUS_ROUTE_NO",
+                "BUS_ROUTE_NM",
+                "STND_BSST_ID",
+                "BSST_ARS_NO",
+                "BUS_STA_NM",
+                "WORK_DT",
+            ])
             df[cols] = df[cols].apply(pd.to_numeric, errors="coerce")
 
         except:
@@ -289,7 +285,9 @@ class TransInfo:
 
             # Open API 서비스 제공처 오류
             else:
-                print(f">>> {te[0].find('CODE').text} {te[0].find('MESSAGE').text}")
+                print(
+                    f">>> {te[0].find('CODE').text} {te[0].find('MESSAGE').text}"
+                )
         # 전체 자료 건 수
         n_data = xmlsoup.findAll("list_total_count")[0].text
         print(f">>> 전체 자료 건 수: {n_data}")
